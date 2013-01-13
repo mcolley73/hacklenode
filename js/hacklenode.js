cfc6c4474911b2b9e5a029b57f25837926ce7535
Mark Colley <mcolley73@gmail.com>
2013-01-12 23:11:58 -0500

    Keeping bangers in the ocean.

--- js/hacklenode.js
+++ js/hacklenode.js
@@ -44,40 +44,65 @@ Hacklenode.prototype = {
 	validateBangerPosition: function(bangerElem){
 		consoleLog("validateBangerPosition(bangerElem)");
 		
-		var top = $(bangerElem).offset().top;
-		var left = $(bangerElem).offset().left;
 		//var top = $(bangerElem).css("top");
 		consoleLog(top);
 		var firstCoord = $(".real-coord", this.ocean)[0];
+		$(firstCoord).css("background-color","orange");
+		var lastCoord = $(".real-coord", this.ocean)[99];
+		$(lastCoord).css("background-color","purple");
+		this.checkTopLeft(firstCoord, $(bangerElem));
+		this.checkBottomRight(lastCoord, $(bangerElem));
+		
+		$(this).removeClass("validating-banger");
+	},
+	
+	checkTopLeft: function(firstCoord, bangerElem){
+		consoleLog("checkTopLeft(firstCoord)");
+
+		var bangerTop = $(bangerElem).offset().top;
+		var bangerLeft = $(bangerElem).offset().left;
+
 		var oceanTop = $(firstCoord).offset().top;
 		var oceanLeft = $(firstCoord).offset().left;
-		//var oceanTop = $(".real-coord", this.ocean)[0];
 		consoleLog(oceanTop);
-		/*
-		if(top < oceanTop){
-			$(bangerElem).position({
-				my:	"top",
-				at:	"top",
-				of:	firstCoord,
-				offset: "0 3"
-			});
-			$(bangerElem).css("top", $(bangerElem).css("top")+5);
-		}
-		*/
-		if(top < oceanTop){
+		if(bangerTop < oceanTop){
 			$(bangerElem).offset({
 				top: oceanTop + 3, 
 				left: $(bangerElem).offset().left
 			});
 		}
-		if(left < oceanLeft){
+		if(bangerLeft < oceanLeft){
 			$(bangerElem).offset({
 				top: $(bangerElem).offset().top, 
 				left: oceanLeft + 4
 			});
 		}
+	},
+	
+	checkBottomRight: function(lastCoord, bangerElem){
+		consoleLog("checkBottomRight(lastCoord,bangerElem)");
 		
-		$(this).removeClass("validating-banger");
+		var bangerOffset = $(bangerElem).offset();
+		var bangerBottom = (bangerOffset.top + bangerElem.outerHeight());
+		var bangerRight = (bangerOffset.left + bangerElem.outerWidth());
+		
+		var offset = $(lastCoord).offset(); //cache the position
+		var oceanBottom = (offset.top + $(lastCoord).outerHeight());
+		var oceanRight = (offset.left + $(lastCoord).outerWidth());
+		
+		consoleLog(oceanBottom);
+		if(bangerBottom > oceanBottom){
+			$(bangerElem).offset({
+				top: oceanBottom - 3 - bangerElem.outerHeight(), 
+				left: bangerElem.left
+			});
+		}
+		if(bangerRight > oceanRight){
+			$(bangerElem).offset({
+				top: bangerElem.top, 
+				left: oceanRight - 4 - bangerElem.outerWidth()
+			});
+		}
 	},
 	
 	addMessage: function(message){
